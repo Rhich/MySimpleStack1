92498dcb070620931baeb60081ec56aaf9d2114c
Rhich <rouishichem@yahoo.fr>
2016-12-04 23:06:53 +0100

    mon premier commit

---
+++ src/main/java/deco.Hrou/App.java
@@ -0,0 +1,13 @@
+package deco.Hrou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
