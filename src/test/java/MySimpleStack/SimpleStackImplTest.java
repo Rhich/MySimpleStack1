3b56784f41bfb7c81840d5392674a7172da1d70d
Rhich <rouishichem@yahoo.fr>
2016-12-05 00:18:05 +0100

    #1 exigance 1

---
+++ src/test/java/MySimpleStack/SimpleStackImplTest.java
@@ -0,0 +1,46 @@
+package MySimpleStack;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import sun.beans.editors.StringEditor;
+
+import static org.junit.Assert.*;
+
+public class SimpleStackImplTest {
+private SimpleStackImpl simpleStack;
+    @Before
+    public void setUp() throws Exception {
+        simpleStack = new SimpleStackImpl();
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+        Assert.assertEquals(true,simpleStack.isEmpty());
+        String test1 = "test";
+        simpleStack.push(new Item(new String (test1)));
+        Assert.assertEquals(false,simpleStack.isEmpty());
+    }
+
+    @Test
+    public void testGetSize() throws Exception {
+        Assert.assertEquals(0,simpleStack.getSize());
+
+
+    }
+
+    @Test
+    public void testPush() throws Exception {
+
+    }
+
+    @Test
+    public void testPeek() throws Exception {
+
+    }
+
+    @Test
+    public void testPop() throws Exception {
+
+    }
+}
